9fd04a608dab1dda2eec5e4ad81233c1e961daa3
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-03-07 09:10:58 -0400

    Fix angry rock

--- Assets/Scripts/bye.cs
+++ Assets/Scripts/bye.cs
@@ -7,10 +7,12 @@ public class bye : MonoBehaviour
 {
     string sceneName = "Angry Rock";
 
+    GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,7 @@ public class bye : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        gameManager.state = GameState.angryRock;
         SceneManager.LoadScene(sceneName);
     }
 }
