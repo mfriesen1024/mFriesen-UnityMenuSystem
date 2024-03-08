085ff959a9f6e74b2efb555680d115b5f1c41ff3
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-03-07 21:21:39 -0400

    Remove debug exception

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -6,21 +6,11 @@ public class LevelManager : MonoBehaviour
 {
     public void LoadScene(string value)
     {
-        try { throw new DebugException(); }
-        catch (DebugException e)
-        {
-            Debug.Log($"A scene change was requested, scene was {value}");
-            Debug.Log(e.StackTrace);
-        }
+        
         //if(value is int) { SceneManager.LoadScene((int)value); }
         if (value is not null) { SceneManager.LoadScene(value); }
         //else { throw new NotImplementedException("Youboring"); }
 
 
     }
-}
-
-public class DebugException : Exception // this is intended to be thrown for debugging purposes in a trycatch.
-{
-    
 }
